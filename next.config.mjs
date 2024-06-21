bec6c007acf0d6c8a2b7408798468dfeb154795f
yasir5405 <yasirnaseem5415@gmail.com>
2024-06-21 19:24:16 +0530

    new fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        ignoreDuringBuilds: true;
+        ignoreDuringBuilds: true
     },
 };
 
